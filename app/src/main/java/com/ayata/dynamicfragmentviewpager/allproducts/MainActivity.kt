fce1cef0360e48c983e4bbfd87e908ac208ce345
ShristySk1 <srezty@gmail.com>
2021-07-18 13:03:20 +0545

    set current tab

--- app/src/main/java/com/ayata/dynamicfragmentviewpager/allproducts/MainActivity.kt
+++ app/src/main/java/com/ayata/dynamicfragmentviewpager/allproducts/MainActivity.kt
@@ -1,25 +1,17 @@
 package com.ayata.dynamicfragmentviewpager.allproducts
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.ayata.dynamicfragmentviewpager.databinding.ActivityMainBinding
 import com.ayata.dynamicfragmentviewpager.allproducts.model.MyServerData
 import com.ayata.dynamicfragmentviewpager.allproducts.model.MyServerDataItem
+import com.ayata.dynamicfragmentviewpager.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity(), (MyServerDataItem) -> Unit {
     lateinit var binding: ActivityMainBinding
-//    val servermodel = ServerModel(
-//        listOf(
-//            Category("Cat1", listOf(Item("1", "cat1name1"), Item("2", "cat1name2"))),
-//            Category("Cat2", listOf(Item("1", "cat2name1"))),
-//            Category("Cat3", listOf(Item("1", "cat3name1"))),
-//            Category("Cat4", listOf(Item("1", "cat4name1")))
-//
-//        ), "2"
-//    )
-
+    val tabTitles: MutableList<String> by lazy { mutableListOf() }
     val servermodel2 = MyServerData(
         listOf(
             MyServerDataItem("cat1", 1, "rice"),
@@ -28,8 +20,6 @@ class MainActivity : AppCompatActivity(), (MyServerDataItem) -> Unit {
             MyServerDataItem("cat3", 4, "vodka"),
             MyServerDataItem("cat1", 5, "pulse"),
             MyServerDataItem("cat4", 6, "blanket")
-
-
         )
     )
     val filteredData = servermodel2.data.groupBy { it.category }
@@ -38,20 +28,6 @@ class MainActivity : AppCompatActivity(), (MyServerDataItem) -> Unit {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //first method
-//        val list = mutableListOf<DummyModel>()
-//        for (i in 0 until servermodel.categories.size) {
-////            binding.tabLayout.addTab(binding.tabLayout.newTab().setText("tab$i"))
-//            list.add(DummyModel(servermodel.categories[i].items))
-//        }
-//        var pagerViewAdapter =
-//            PagerAdapter(this, list)
-//        binding.viewpager.adapter = pagerViewAdapter
-//        TabLayoutMediator(binding.tabLayout, binding.viewpager) { tab, position ->
-//            tab.text = servermodel.categories[position].categoryName
-//        }.attach()
-
-
         //next method
         val list = mutableListOf<DummyModel2>()
         filteredData.forEach { entry ->
@@ -60,24 +36,30 @@ class MainActivity : AppCompatActivity(), (MyServerDataItem) -> Unit {
         var pagerViewAdapter =
             PagerAdapter2(this, list, this)
         binding.viewpager.adapter = pagerViewAdapter
-        TabLayoutMediator(binding.tabLayout, binding.viewpager) { tab, position ->
+        TabLayoutMediator(binding.tabLayout, binding.viewpager, false, true) { tab, position ->
             tab.text = filteredData.keys.elementAt(position)
+            tabTitles.add(filteredData.keys.elementAt(position))
         }.attach()
+        //to set certain tab selected according to tab title
+        getBundle()
 
-//common not needed
-//        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-//            override fun onTabReselected(p0: TabLayout.Tab?) {
-//
-//            }
-//
-//            override fun onTabSelected(p0: TabLayout.Tab?) {
-//                binding.viewpager.currentItem = p0!!.position
-//            }
-//
-//            override fun onTabUnselected(p0: TabLayout.Tab?) {
-//
-//            }
-//        })
+
+    }
+
+    fun getBundle() {
+        val alreadySelectedCategory = "cat3"
+        val pos = tabTitles.indexOf(alreadySelectedCategory)
+        Log.d("getBundle", "getBundle: " + pos);
+        /**
+         *         here smooth scroll takes place
+         */
+//        binding.tabLayout.setScrollPosition(pos, 0f, false)
+//        binding.tabLayout.getTabAt(pos)?.select();
+//        binding.tabLayout.selectTab(binding.tabLayout.getTabAt(pos))
+        /**
+         *         here smooth scroll does not takes place
+         */
+        binding.viewpager.setCurrentItem(pos, false);
 
     }
 
